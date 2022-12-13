a796e7b08711092b15d1311e9dfc7baa0babe7b0
Johan Dahl <Johan.Dahl@lth.lu.se>
2022-12-13 09:44:13 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="hello">Hello</button>
+  </div>
+</template>
+
+<script>
+  import api from './api/index.js'
+
+  export default {
+    name: 'App',
+    methods: {
+      hello () {
+        console.log('App.hello')
+        api.greetings.hello()
+      }
+    }
+  }
+</script>
---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import * as greetings from './greetings.js'
+
+export default {
+  greetings
+}
